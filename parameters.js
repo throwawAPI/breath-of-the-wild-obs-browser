5c3ba72f4621175cb9158491daf400f2358c790b
ThrowawAPI <throwawAPI+github@gmail.com>
2021-05-06 12:39:27 -0400

    Introducing: THEMES. - use &theme=light or &theme=dark URL parameters - TODO: need to make SVGs for the stamina_1 thru stamina_4 icons

--- parameters.js
+++ parameters.js
@@ -3,39 +3,50 @@ window.onload = function() {
   const query  = window.location.search;
   const params = new URLSearchParams(query);
   // set defaults in case no parameter is offered
-  var hearts     = parseInt(params.get('hearts')     || '3');
-  var stamina    = parseInt(params.get('stamina')    || '5');
-  var spiritOrbs =          params.get('spiritorbs') || '0';
-  var shrines    =          params.get('shrines')    || '0';
-  var korokSeeds =          params.get('korokseeds') || '0';
-  var rupees     = parseInt(params.get('rupees')     || '0');
+  var theme      =          params.get("theme")      || "light";
+  var hearts     = parseInt(params.get("hearts")     || "3");
+  var stamina    = parseInt(params.get("stamina")    || "5");
+  var spiritOrbs =          params.get("spiritorbs") || "0";
+  var shrines    =          params.get("shrines")    || "0";
+  var korokSeeds =          params.get("korokseeds") || "0";
+  var rupees     = parseInt(params.get("rupees")     || "0");
   // generate a few objects which can be duplicated if needed
+  switch (theme) {
+    case "light":
+    case "dark":
+      break;
+    default:
+      theme = "light";
+  }
   var imgHeart = new Image();
-  imgHeart.src = "icons/dark-transparent/heart.png";
+  imgHeart.src = `icons/${theme}/heart.svg`;
   imgHeart.alt = "heart";
   imgHeart.className = "heart";
   var imgStamina1 = new Image();
-  imgStamina1.src = "icons/dark-transparent/stamina_1.png";
+  imgStamina1.src = `icons/${theme}/stamina_5.svg`;
   imgStamina1.alt = "stamina wheel with 1 segment";
   imgStamina1.className = "stamina-wheel stamina-1";
   var imgStamina2 = new Image();
-  imgStamina2.src = "icons/dark-transparent/stamina_2.png";
+  imgStamina2.src = `icons/${theme}/stamina_5.svg`;
   imgStamina2.alt = "stamina wheel with 2 segment";
   imgStamina2.className = "stamina-wheel stamina-2";
   var imgStamina3 = new Image();
-  imgStamina3.src = "icons/dark-transparent/stamina_3.png";
+  imgStamina3.src = `icons/${theme}/stamina_5.svg`;
   imgStamina3.alt = "stamina wheel with 3 segment";
   imgStamina3.className = "stamina-wheel stamina-3";
   var imgStamina4 = new Image();
-  imgStamina4.src = "icons/dark-transparent/stamina_4.png";
+  imgStamina4.src = `icons/${theme}/stamina_5.svg`;
   imgStamina4.alt = "stamina wheel with 4 segment";
   imgStamina4.className = "stamina-wheel stamina-4";
   var imgStamina5 = new Image();
-  imgStamina5.src = "icons/dark-transparent/stamina_5.png";
+  imgStamina5.src = `icons/${theme}/stamina_5.svg`;
   imgStamina5.alt = "stamina wheel with 5 segment";
   imgStamina5.className = "stamina-wheel stamina-5";
   // fill images for health and stamina
   // alter span.innerHTML for text fields
+  // // BODY
+  var body = document.querySelector("body");
+  body.classList.add(`theme-${theme}`)
   // // HEARTS
   var divHearts = document.querySelector("div.hearts");
   while (divHearts.firstChild) {
@@ -74,15 +85,23 @@ window.onload = function() {
     }
   }
   // // SPIRIT ORBS
-  var spanSpiritOrbs = document.querySelector("span.spirit-orbs-text");
+  var imgSpiritOrb         = document.querySelector("img.spirit-orb");
+  imgSpiritOrb.src         = `icons/${theme}/spirit_orb.svg`;
+  var spanSpiritOrbs       = document.querySelector("span.spirit-orbs-text");
   spanSpiritOrbs.innerHTML = spiritOrbs;
   // // SHRINES
-  var spanShrines = document.querySelector("span.shrines-text");
+  var imgShrine         = document.querySelector("img.shrine");
+  imgShrine.src         = `icons/${theme}/shrine.svg`;
+  var spanShrines       = document.querySelector("span.shrines-text");
   spanShrines.innerHTML = shrines;
   // // KOROK SEEDS
-  var spanKorokSeeds = document.querySelector("span.korok-seeds-text");
+  var imgKorokSeed         = document.querySelector("img.korok-seed");
+  imgKorokSeed.src         = `icons/${theme}/korok_seed.svg`;
+  var spanKorokSeeds       = document.querySelector("span.korok-seeds-text");
   spanKorokSeeds.innerHTML = korokSeeds;
   // // RUPEES
-  var spanRupees = document.querySelector("span.rupees-text");
+  var imgRupee         = document.querySelector("img.rupee");
+  imgRupee.src         = `icons/${theme}/rupee.svg`;
+  var spanRupees       = document.querySelector("span.rupees-text");
   spanRupees.innerHTML = rupees.toLocaleString();
 }
